2030be4ac5eb27a65f48dbe127c6d0e1f799243c
游艺(youyi) <youyichannel@163.com>
2024-04-08 22:14:20 +0800

    feat(bili_video): display bili video

---
+++ searchhub-platform/src/components/BiliVideoList.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { BiliVideo } from "../model/biliVideo";
+import { withDefaults } from "vue";
+
+interface Props {
+  biliVideoList: BiliVideo[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  biliVideoList: () => [],
+});
+</script>
+
+<template>
+  <a-list
+    item-layout="horizontal"
+    :grid="{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 3, xl: 4, xxl: 3 }"
+    :data-source="props.biliVideoList"
+  >
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <a :href="item.videoUrl">
+          <a-card>
+            <template #cover>
+              <img :src="item.pic" referrerpolicy="no-referrer" />
+            </template>
+            <template #title>
+              <span v-html="item.title"></span>
+            </template>
+          </a-card>
+        </a>
+      </a-list-item>
+    </template>
+  </a-list>
+</template>
+
+<style scoped></style>
