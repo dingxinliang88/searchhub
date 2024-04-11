cd6e443fb46b3455104713753c3301dc653a2f71
游艺(youyi) <youyichannel@163.com>
2024-04-11 16:57:44 +0800

    feat(retry): support retry when send http request

--- searchhub-server/src/main/java/com/youyi/searchhub/core/datasource/BiliVideoDataSource.java
+++ searchhub-server/src/main/java/com/youyi/searchhub/core/datasource/BiliVideoDataSource.java
@@ -10,6 +10,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.youyi.searchhub.model.entity.BiliVideo;
 import com.youyi.searchhub.model.enums.SearchType;
 import com.youyi.searchhub.model.vo.BiliVideoVO;
+import com.youyi.searchhub.util.RetryUtil;
 import java.net.HttpCookie;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,38 +37,46 @@ public class BiliVideoDataSource implements DataSource<BiliVideoVO> {
 
     @Override
     public Page<BiliVideoVO> doSearch(String searchText, long current, long pageSize) {
-        List<BiliVideoVO> biliVideoVOList = new ArrayList<>();
-        try (HttpResponse httpResponse = HttpRequest.get(
+        String body;
+        try (HttpResponse response = RetryUtil.retry(() -> HttpRequest.get(
                         String.format(FETCH_BILI_VIDEO_URL, searchText))
                 .cookie(getBiliCookie())
-                .execute()) {
-            String body = httpResponse.body();
-            Map<String, Object> bodyMap = JSONUtil.toBean(body, new TypeReference<>() {
-            }, true);
-            Optional<JSONObject> optionalData = Optional.ofNullable(
-                    (JSONObject) bodyMap.get("data"));
-            optionalData.ifPresent(data -> {
-                JSONArray result = data.getJSONArray("result");
-                for (Object res : result) {
-                    JSONObject jsonObject = (JSONObject) res;
-
-                    BiliVideo biliVideo = new BiliVideo();
-                    biliVideo.setTitle(jsonObject.getStr("title"));
-                    biliVideo.setDescription(jsonObject.getStr("description"));
-                    biliVideo.setPic("https:" + jsonObject.getStr("pic"));
-                    biliVideo.setVideoUrl(jsonObject.getStr("arcurl"));
-                    biliVideoVOList.add(biliVideo.toVO());
-                }
-            });
+                .execute(), 5)) {
+            body = response.body();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        Page<BiliVideoVO> biliVideoVOPage = new Page<>(BILI_VIDEO_CURRENT, BILI_VIDEO_PAGE_SIZE);
-        biliVideoVOPage.setRecords(biliVideoVOList);
-        return biliVideoVOPage;
+        return getBiliVideoPage(body);
     }
 
     @Override
     public String getType() {
         return SearchType.BILI_VIDEO.getType();
+    }
+
+
+    private Page<BiliVideoVO> getBiliVideoPage(String body) {
+        List<BiliVideoVO> biliVideoVOList = new ArrayList<>();
+        Map<String, Object> bodyMap = JSONUtil.toBean(body, new TypeReference<>() {
+        }, true);
+        Optional<JSONObject> optionalData = Optional.ofNullable(
+                (JSONObject) bodyMap.get("data"));
+        optionalData.ifPresent(data -> {
+            JSONArray result = data.getJSONArray("result");
+            for (Object res : result) {
+                JSONObject jsonObject = (JSONObject) res;
+
+                BiliVideo biliVideo = new BiliVideo();
+                biliVideo.setTitle(jsonObject.getStr("title"));
+                biliVideo.setDescription(jsonObject.getStr("description"));
+                biliVideo.setPic("https:" + jsonObject.getStr("pic"));
+                biliVideo.setVideoUrl(jsonObject.getStr("arcurl"));
+                biliVideoVOList.add(biliVideo.toVO());
+            }
+        });
+        Page<BiliVideoVO> biliVideoVOPage = new Page<>(BILI_VIDEO_CURRENT, BILI_VIDEO_PAGE_SIZE);
+        biliVideoVOPage.setRecords(biliVideoVOList);
+        return biliVideoVOPage;
     }
 
     private HttpCookie getBiliCookie() {
--- searchhub-server/src/main/java/com/youyi/searchhub/core/datasource/NewsDataSource.java
+++ searchhub-server/src/main/java/com/youyi/searchhub/core/datasource/NewsDataSource.java
@@ -11,6 +11,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.youyi.searchhub.model.entity.News;
 import com.youyi.searchhub.model.enums.SearchType;
 import com.youyi.searchhub.model.vo.NewsVO;
+import com.youyi.searchhub.util.RetryUtil;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,51 +38,57 @@ public class NewsDataSource implements DataSource<NewsVO> {
 
     @Override
     public Page<NewsVO> doSearch(String searchText, long current, long pageSize) {
-        long total;
-        List<NewsVO> newsVOList = new ArrayList<>();
-        try (HttpResponse httpResponse = HttpRequest.get(FETCH_NEWS_URL)
+        String originRes;
+        try (HttpResponse httpResponse = RetryUtil.retry(() -> HttpRequest.get(FETCH_NEWS_URL)
                 .header(Header.USER_AGENT, USER_AGENT)
                 .header(Header.REFERER, REFERER)
-                .execute()) {
-            String originRes = httpResponse.body();
-            Matcher matcher = CONTENT_PATTERN.matcher(originRes);
-            if (!matcher.find()) {
-                throw new RuntimeException("获取新闻数据失败");
-            }
-
-            String contentJson = matcher.group(1); // 内容 JSON
-            Map<String, Object> contentMap = JSONUtil.toBean(contentJson, new TypeReference<>() {
-            }, true);
-
-            JSONObject data = (JSONObject) contentMap.get("data");
-
-            total = data.getLong("total");
-            JSONArray dataList = data.getJSONArray("list");
-
-            for (Object o : dataList) {
-                JSONObject item = (JSONObject) o;
-                String title = item.getStr("title");
-                String url = item.getStr("url");
-                String brief = item.getStr("brief");
-                String keywords = item.getStr("keywords");
-                String image = item.getStr("image");
-                LocalDateTime focusDate = item.getLocalDateTime("focus_date", LocalDateTime.now());
-                News news = new News(title, url, brief, keywords, image, focusDate);
-                newsVOList.add(news.toVO());
-
-                if (newsVOList.size() >= pageSize) {
-                    break;
-                }
-            }
+                .execute(), 5)) {
+            originRes = httpResponse.body();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-
-        Page<NewsVO> newsVOPage = new Page<>(current, pageSize, total);
-        newsVOPage.setRecords(newsVOList);
-        return newsVOPage;
+        return getNewsVOPage(originRes, current, pageSize);
     }
 
     @Override
     public String getType() {
         return SearchType.NEWS.getType();
     }
+
+    private Page<NewsVO> getNewsVOPage(String originRes, long current, long pageSize) {
+        List<NewsVO> newsVOList = new ArrayList<>();
+        Matcher matcher = CONTENT_PATTERN.matcher(originRes);
+        if (!matcher.find()) {
+            throw new RuntimeException("获取新闻数据失败");
+        }
+
+        String contentJson = matcher.group(1); // 内容 JSON
+        Map<String, Object> contentMap = JSONUtil.toBean(contentJson, new TypeReference<>() {
+        }, true);
+
+        JSONObject data = (JSONObject) contentMap.get("data");
+
+        long total = data.getLong("total");
+        JSONArray dataList = data.getJSONArray("list");
+
+        for (Object o : dataList) {
+            JSONObject item = (JSONObject) o;
+            String title = item.getStr("title");
+            String url = item.getStr("url");
+            String brief = item.getStr("brief");
+            String keywords = item.getStr("keywords");
+            String image = item.getStr("image");
+            LocalDateTime focusDate = item.getLocalDateTime("focus_date", LocalDateTime.now());
+            News news = new News(title, url, brief, keywords, image, focusDate);
+            newsVOList.add(news.toVO());
+
+            if (newsVOList.size() >= pageSize) {
+                break;
+            }
+        }
+        Page<NewsVO> newsVOPage = new Page<>(current, pageSize, total);
+        newsVOPage.setRecords(newsVOList);
+        return newsVOPage;
+    }
+
 }
